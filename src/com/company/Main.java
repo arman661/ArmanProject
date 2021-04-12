ebbe1be7cf9e6a7f6bae21892c01fefd06561cc5
okkon811 <okkon811@gmail.com>
2021-04-12 18:46:59 +0300

    create factorial() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(multiply(2, 5, 10));
+        System.out.println(factorial(32));
     }
     public static int multiply(int... numbers) {
         if(numbers.length <1) {
@@ -13,6 +14,16 @@ public class Main {
         int result = 1;
         for (int number : numbers) {
             result *= number;
+        }
+        return result;
+    }
+    public static  int factorial(int number) {
+        if(number < 0) {
+            throw new IllegalArgumentException("Введенное число меньше 0");
+        }
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
         }
         return result;
     }
